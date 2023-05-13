a0b8d4be3f563c1de25dbbf89dc2a0a59005dc85
Axel Padilla <adpadillar25@gmail.com>
2023-05-12 23:40:35 -0600

    readingtime ts

--- src/utils/readingTime.ts
+++ src/utils/readingTime.ts
@@ -2,7 +2,7 @@ function readingTime(content: string) {
   const wpm = 200;
   const words = content.trim().split(/\s+/).length;
   const time = Math.ceil(words / wpm);
-  return time + " min read";
+  return `${time.toString()} min read` as const;
 }
 
 export default readingTime;
